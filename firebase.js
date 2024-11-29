57699b4d7df5a0c900cae14d94ed896ca640a5b6
yuvrajjaat <130272717+yuvrajjaat@users.noreply.github.com>
2024-11-29 23:05:07 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDau3RNBmGjggsWMH-XFjA4DrpaspLdmq0",
+  authDomain: "rbac-vrv-security-d7afd.firebaseapp.com",
+  projectId: "rbac-vrv-security-d7afd",
+  storageBucket: "rbac-vrv-security-d7afd.firebasestorage.app",
+  messagingSenderId: "273039118922",
+  appId: "1:273039118922:web:f523421cb155c2d4b44cb3",
+  measurementId: "G-SQ3Z4BQMQB"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
